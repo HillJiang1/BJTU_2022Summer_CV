ee117da199de21c8c07e653d05e11cae46db5677
HillJiang <hilljiang2001@gmail.com>
2022-07-04 11:36:43 +0800

    EnvTest

---
+++ envTest.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import keras
+import cv2
+import dlib
+import face_recognition
+
+
+
+print("tensorflow version",tf.__version__)
+print("cv2 version:",cv2.__version__)
+print("keras version:",keras.__version__)
+print("dlib version:",dlib.__version__)
+print("face_recognition:",face_recognition.__version__)
